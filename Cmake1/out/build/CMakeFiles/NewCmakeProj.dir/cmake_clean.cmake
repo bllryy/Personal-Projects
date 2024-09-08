5e0a46142eea8d9bf4515c620334c7cc1ca98836
bllry <fingoscha@gmail.com>
2024-09-08 14:32:22 -0400

    Cmake1

---
+++ Cmake1/out/build/CMakeFiles/NewCmakeProj.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewCmakeProj.dir/main.cpp.o"
+  "CMakeFiles/NewCmakeProj.dir/main.cpp.o.d"
+  "NewCmakeProj"
+  "NewCmakeProj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewCmakeProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
