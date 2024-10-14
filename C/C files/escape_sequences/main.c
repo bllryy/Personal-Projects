c5d25135b79e0ebf8f844e394b6e3eaa8a8fef0f
bllry <GoschaF26@chcstudent.com>
2024-10-13 22:41:35 -0400

    C

---
+++ C/C files/escape_sequences/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    /*
+    escape sequ. - character combination consisting of a backlash \ 
+    followed by a letter or combination of digits
+    they specify actions wiuthin a line or a sting of text
+
+    \n = newline 
+    \t = tab
+    */
+
+   printf("\n OwO \n UwU\n");
+   return 0;
+}
---
+++ C/C files/vars/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    // var = allocated space in memory to store a vlaue
+
+    int x;  // declaration
+    x = 123;    // set equal
+    int y = 321;
+
+    int age = 21;  
+    float gpa = 2.05;
+    char grade = 'C';
+    char name[] = "Fin";
+    printf("Hello %s\n", name);
+    printf("You are %d years old",age);
+
+    return 0;
+}
