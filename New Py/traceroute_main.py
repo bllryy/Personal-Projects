09683ae7b677a2b448d30c584a0cbf9d5a95a990
bllryy <brickbybrick406@gmail.com>
2024-11-07 13:14:00 -0500

    Nov7_Update

--- New Py/traceroute_main.py
+++ New Py/traceroute_main.py
@@ -1,3 +1,25 @@
+"""
+ADD name and stuff
+
+
+"""
+
+"""
+Approach:
+Get the empty arrays for hops and max hops
+    also ttl if it dosent work
+Spawn a send socket that sends the initial ping 
+ping comes back and hits the recieve socket
+print what we get back in the format
+also web version in a website to get the line limit ( ͡° ͜ʖ ͡°) 
+
+"""
+
+
+
+
+
+
 import socket
 import struct
 import time
@@ -7,12 +29,20 @@ import string
 import sys
 
 
+
 def tracroute(destination, max_jumps=30, callout=5):    # callout is for timeout
     # hops
     hops = []
     # max hops
     max_hops = 30
+    # TTL
+    ttl = 5
 
     # create socket
-    
+    receivesocket = socket.socket() # NEED STUFF IN HERE
+    sendsocket = socket.socket()    # ^^^^
+
+    # Set the ttl of the socket
+    sendsocket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+
 
