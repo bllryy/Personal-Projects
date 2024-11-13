a7de47c11c41bfb56b699ba10fc6c1098cc053bc
bllryy <GoschaF26@chcstudent.com>
2024-11-12 21:05:15 -0500

    Nov12

--- New Py/traceroute_main.py
+++ New Py/traceroute_main.py
@@ -1,6 +1,7 @@
 """
-ADD name and stuff
-
+Name: 
+Section: 275-02
+Desc: Project 1
 
 """
 
@@ -11,38 +12,56 @@ Get the empty arrays for hops and max hops
 Spawn a send socket that sends the initial ping 
 ping comes back and hits the recieve socket
 print what we get back in the format
-also web version in a website to get the line limit ( ͡° ͜ʖ ͡°) 
+also web version in a website to get the line limit
 
+
+while loop goes over and prints first location, second location, third, etc...
+while ttl <= max_hops do while
 """
-
-
-
-
 
 
 import socket
 import struct
 import time
-from scapy.all import *
+#from scapy.all import *
 import argparse
 import string
 import sys
 
 
 
-def tracroute(destination, max_jumps=30, callout=5):    # callout is for timeout
+def tracroute(destination, max_hops=30, timeout=5):
+    destination = input("Enter the IP address or domain name of the destination: ")
+    tracroute(destination)
+
     # hops
     hops = []
-    # max hops
-    max_hops = 30
-    # TTL
     ttl = 5
 
-    # create socket
-    receivesocket = socket.socket() # NEED STUFF IN HERE
-    sendsocket = socket.socket()    # ^^^^
-
-    # Set the ttl of the socket
-    sendsocket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+    #get ip
+    dest_ip = socket.gethostbyname(destination)
+    print(f"Traceroute to {destination} ({dest_ip}), max {max_hops} hops:")    
 
 
+
+    while ttl <= max_hops:
+
+
+
+        # create send/recieve socket
+        recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+        # Set the ttl of the socket/options?????
+        send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+        recv_socket.settimeout(timeout)
+
+        # send packet
+        print(f"Sending packet with TTL = {ttl}...")
+        start_time = time.time()
+        send_socket.sendto(b'', (dest_ip), 33434) # port is common??????
+
+
+
+if __name__ == "__main__":
+    tracroute()
